2be3363f1d582e51ce4629396d32df7c0bb87529
Shosetsu <romamarin16@gmail.com>
2021-07-30 23:57:56 +0300

    Portfolio #13.5

--- Portfolio/JS/app.js
+++ Portfolio/JS/app.js
@@ -2,6 +2,47 @@ $(function(){
 
     const worksSlider = $('[data-slider="slick"]');
 
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop;
+
+
+
+    // Fixed Header
+
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if ( scrollOffset > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+    // Smooth scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+
+
+
     // Filter
 
     let filter = $("[data-filter]");
